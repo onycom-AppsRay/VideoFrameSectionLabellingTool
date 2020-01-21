a0a7300268e36ac1a13763e2646192bf4c0e43a0
parkyounghwan <dudrnxps1@gmail.com>
2020-01-21 21:05:05 +0900

    #master, canvas labelling

--- app/components/file/file.js
+++ app/components/file/file.js
@@ -1,5 +1,8 @@
 const dirTree = require('directory-tree');
 
+const SELECTED_FILE_TAG = 'file-list';
+const COMPLITED_FILE_TAG = 'completed-file-list';
+
 function onDirectorySelect(e) {
   const directoryPath = e.target.files[0].path;
 
@@ -8,11 +11,11 @@ function onDirectorySelect(e) {
   dirSearch(tree);
 }
 
-function dirSearch (tree) {
-  const fileTree = document.getElementById('file-tree');
-  
+function dirSearch(tree) {
+  const fileTree = document.getElementById(SELECTED_FILE_TAG);
+
   tree.children.forEach(element => {
-    if(element.type == 'file') {
+    if (element.type == 'file') {
       const file = document.createElement('a');
       const filePath = document.createTextNode(element.name);
       file.setAttribute('href', '#');
@@ -27,3 +30,31 @@ function dirSearch (tree) {
     }
   });
 }
+
+/**
+ * file name 확인 해서 '완료된 파일' 부분에 보여주기
+ * 1. p tag 에 파일 이름 존재하는지 확인
+ * 2. 불러온 파일 리스트를 전역으로 하나 관리해서, 완료된 파일은 내리거나 색 처리
+ * 3. '완료된 파일'을 선택 시, 다시 볼 수 있도록
+ */
+function onSuccess() {
+  const fileName = document.getElementById('file-name').innerText;
+
+  // file-name innerText 존재 유무 확인 절차 구현할 것
+  console.log(fileName);
+
+  const fileTree = document.getElementById(COMPLITED_FILE_TAG);
+
+  const file = document.createElement('a');
+  const filePath = document.createTextNode(fileName);
+  file.setAttribute('href', '#');
+  file.setAttribute('onclick', 'onVideoSelected(event)');
+  // file.setAttribute('id', element.path);
+
+  const p = document.createElement('br');
+
+  file.appendChild(filePath);
+  fileTree.appendChild(file);
+  fileTree.appendChild(p);
+}
+
--- app/components/video/video-util.js
+++ app/components/video/video-util.js
@@ -1,8 +1,10 @@
-var cv = require('opencv4nodejs');
+const cv = require('opencv4nodejs');
 
-let frameList;
+const frameList = [];
 
 function onVideoSelected(e) {
+  console.log(e.target.innerText);
+
   const filePath = e.target.id;
 
   frameList = [];
@@ -20,7 +22,7 @@ function onVideoSelected(e) {
     frame = vCap.read();
     if(frame.empty) break;
 
-    const frame2 = frame.resizeToMax(300);
+    const frame2 = frame.resizeToMax(200);
 
     frameList.push(frame2);
     
@@ -29,7 +31,7 @@ function onVideoSelected(e) {
     canvas.setAttribute('id', `${i}`);
     canvas.setAttribute('onclick', 'onSelectedFrame(event)');
 
-    const container = document.getElementById('frame-flow');
+    const container = document.getElementById('sub-frame');
     container.appendChild(canvas);
 
     renderImage(frame2, document.getElementById(`${i}`));
@@ -43,9 +45,20 @@ function onVideoSelected(e) {
 }
 
 function onSelectedFrame(e) {
-  const frameIdx = e.target.getAttribute('id');
+  const frameIdx = e.target.getAttribute('id'); // tag 'id' option 을 통해서, index 가져옴
 
   renderImage(frameList[frameIdx], document.getElementById('mask'));
   
-  document.getElementById('frame-number').innerText = `Selected Frame Index: ${idx}`;
+  document.getElementById('frame-number').innerText = `Selected Frame Index: ${frameIdx}`;
+
+  const frame = document.getElementById('frame-number');
+  const backgroundColor = frame.style.backgroundColor;
+
+  console.log('onSelectedFrame() - backgroundColor: ', backgroundColor);
+
+  if(backgroundColor === '') {
+    frame.style.backgroundColor = 'black';
+  } else {
+    frame.style.backgroundColor = '';
+  }
 }
